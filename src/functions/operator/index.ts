19589c6d385cd8b9a8616a0ff831e737c9bdeea6
Julio Sansossio <juliosansossio@gmail.com>
2024-06-14 21:03:18 -0400

    refactor

--- src/functions/operator/index.ts
+++ src/functions/operator/index.ts
@@ -1,22 +1,27 @@
 import { handlerPath } from '@/libs/handler-resolver';
 
+const events: any = [
+  {
+    http: {
+      method: 'post',
+      path: 'operator',
+    }
+  }
+];
+
+if (process.env.NODE_ENV !== 'local') {
+  events.push({
+    sqs: {
+      arn: {
+        'Fn::GetAtt': ['SolesbotOperateQueue', 'Arn'],
+      },
+    },
+  });
+}
+
 export default {
   handler: `${handlerPath(__dirname)}/handler.main`,
   timeout: 10 * 60, // 10 minutes
   memorySize: 128,
-  events: [
-    {
-      sqs: {
-        arn: {
-          'Fn::GetAtt': ['SolesbotOperateQueue', 'Arn'],
-        },
-      },
-    },
-    {
-      http: {
-        method: 'post',
-        path: 'operator',
-      },
-    }
-  ],
+  events,
 };
