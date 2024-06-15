34bd301ffbefdee0288ca82ac2b701f84999a75f
Julio Sansossio <juliosansossio@gmail.com>
2024-06-15 12:12:27 -0400

    adjust rois

--- src/solesbot/strategy/list/get-strategy.ts
+++ src/solesbot/strategy/list/get-strategy.ts
@@ -16,44 +16,44 @@ export const Strategies = new Map<number, SolesbotStrategy>([
       {
         coin: SolesBotCoins.CAKE,
         amount: 30,
-        minProfit: 0.05
+        minProfit: 1.52
       },
       {
         coin: SolesBotCoins.Chainlink,
         amount: 500,
-        minProfit: 0.05
+        minProfit: 0.86
       },
       {
         coin: SolesBotCoins.Avalanche,
         amount: 500,
-        minProfit: 0.05,
+        minProfit: 0.75,
         fill: true
       },
       {
         coin: SolesBotCoins.Fantom,
         amount: 100,
-        minProfit: 0.05
+        minProfit: 1.60
       },
       {
         coin: SolesBotCoins.ARBITRUM,
         amount: 300,
-        minProfit: 0.05
+        minProfit: 0.81
       },
       {
         coin: SolesBotCoins.Lido,
         amount: 300,
-        minProfit: 0.05
+        minProfit: 0.60
       },
       {
         coin: SolesBotCoins.VeChain,
         amount: 300,
-        minProfit: 0.05,
+        minProfit: 0.55,
         fill: true
       },
       {
         coin: SolesBotCoins.Cosmos,
         amount: 1000,
-        minProfit: 0.05,
+        minProfit: 0.74,
         fill: true
       }
     ],
